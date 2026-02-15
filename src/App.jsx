3b29d019ce8087ede218c91606abd87cc6ec7764
jiamori <lifesunny719@gmail.com>
2026-02-16 00:26:32 +0800

    Products/Product page finish - Lv2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { RouterProvider, createHashRouter } from "react-router";
 import routes from './routes/router';
 
+import axios from "axios";
+import * as bootstrap from 'bootstrap';
+import "./sass/all.scss";
+
 const router = createHashRouter(routes);
 
 function App() {
--- src/views/Front/SingleProduct.jsx
+++ src/views/Front/SingleProduct.jsx
@@ -1,7 +1,98 @@
-const SingleProducts = () => {
-  return (<>
-    商品詳細頁
-  </>)
-}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router"
 
-export default SingleProducts
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
+
+const SingleProduct = () => {
+  const [ tempProduct, setTempProduct ] = useState({});
+  const params = useParams();
+  const { id } = params;
+  
+
+  const handleViewMore = async (id) => {
+    try {
+      const url = `${API_BASE}/api/${API_PATH}/product/${id}`;
+      const res = await axios.get(url);
+      setTempProduct(res.data.product);
+    } catch (error) {
+      console.log('取得產品資料失敗：', error);
+    }
+  }
+
+  useEffect(() => {
+    handleViewMore(id);
+  }, [])
+
+  return (<>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-8 mb-3 ">
+          { tempProduct ?
+          (<div className="card">
+            <div className="card-body mx-auto py-4">
+              <img src={ tempProduct.imageUrl } 
+              className="img-fluid img-thumbnail mx-auto mb-3"
+               style={{width: 600}}
+              alt="product photo"/>
+              <h5 className="bg-secondary px-2 py-1 mb-3">商品詳細</h5>
+              <h5 className="card-title mb-3">{ tempProduct.title }</h5>
+              <p className="card-text">
+                分類：{ tempProduct.category}
+              </p>
+              <p className="card-text">
+                風格{ tempProduct.style}
+              </p>
+              <p className="card-text">
+                顏色{ tempProduct.color}
+              </p>
+              <p className="card-text">
+                材質{ tempProduct.material}
+              </p>
+              <p className="card-text">
+                商品描述：{ tempProduct.description}
+              </p>
+              <p className="card-text">
+                價格： { tempProduct.price } 元
+              </p>
+              <p className="card-text">
+                單位： { tempProduct.unit }
+              </p>
+            </div>
+          </div>)
+          : (<p>尚無產品資料</p>)}
+        </div> 
+        <div className="col-md-4">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="bg-secondary px-2 py-1 mb-3">商品簡介</h5>
+              <div className="mb-3 px-2">
+                <h6 className="card-title mb-2">{ tempProduct.title }</h6>
+                <p className="card-text mb-1">
+                  分類：{ tempProduct.category}
+                </p>
+                <p className="card-text mb-1">
+                  風格：{ tempProduct.style}
+                </p>
+              </div>
+              <h5 className="bg-secondary px-2 py-1 mb-3">商品故事</h5>
+              <div className="mb-3 px-2">
+                <h6 className="card-title">{tempProduct.content.story_title}</h6>
+                <p className="card-subtitle mb-2 text-body-secondary">「{tempProduct.content.story_content}」</p>
+              </div>
+              <hr/>
+              <div>
+                <button type="button" className="btn btn-outline-primary me-2">加入購物車</button>
+                <button type="button" className="btn btn-primary">直接購買</button>
+              </div>
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    {/* {JSON.stringify(tempProduct)} */}
+  </>)
+  }
+
+export default SingleProduct
